f6cb77348480ce2efb1f8b292e85b88d12164c9f
Carlos O'Ryan <coryan@google.com>
2026-08-27 17:34:23 +0000

    cleanup: rename `packages/*` directories  (#548)
    Make the directories match their future split repositories.

--- Package.swift
+++ Package.swift
@@ -24,9 +24,9 @@ let package = Package(
     .library(name: "GoogleLongRunning", targets: ["GoogleLongRunning"])
   ],
   dependencies: [
-    .package(path: "../../packages/auth"),
-    .package(path: "../../packages/gax"),
-    .package(path: "../../packages/wkt"),
+    .package(path: "../../packages/swift-google-auth"),
+    .package(path: "../../packages/swift-google-gax"),
+    .package(path: "../../packages/swift-google-wkt"),
     .package(path: "../../generated/swift-google-rpc"),
     .package(url: "https://github.com/apple/swift-log", from: "1.12.0"),
   ],
@@ -34,9 +34,9 @@ let package = Package(
     .target(
       name: "GoogleLongRunning",
       dependencies: [
-        .product(name: "GoogleCloudAuth", package: "auth"),
-        .product(name: "GoogleCloudGax", package: "gax"),
-        .product(name: "GoogleCloudWKT", package: "wkt"),
+        .product(name: "GoogleCloudAuth", package: "swift-google-auth"),
+        .product(name: "GoogleCloudGax", package: "swift-google-gax"),
+        .product(name: "GoogleCloudWKT", package: "swift-google-wkt"),
         .product(name: "GoogleRpc", package: "swift-google-rpc"),
         .product(name: "Logging", package: "swift-log"),
       ],
